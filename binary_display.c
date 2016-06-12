26e90a53f3443e9c2e9ce3c4040ac9fad8e37475
w.glanzer <wernerglanzer@googlemail.com>
2016-06-12 12:21:50 +0200

    Binäres Display

---
+++ binary_display.c
@@ -0,0 +1,49 @@
+#include "binary_display.h"
+
+float __bin_displaynumber;
+
+/**
+ * Setzt die Zahl, die angezeigt werden soll.
+ * Kommastellenbehandlung:
+ * Erste Kommastelle >= 5 -> LED an
+ * Erste Kommastelle < 5  -> LED aus
+ * 
+ * @param pNumber Zahl, die angezeigt werden soll
+ */
+void bdisplay_set(float pNumber)
+{
+  __bin_displaynumber = pNumber;
+}
+
+/**
+ * Setzt die OUTPUT-Pins auf den nötigen Zustand,
+ * damit die mit bidsplay_set gesetzte Zahl angezeigt wird
+ */
+void bdisplay_loop()
+{
+  _bdisplay_reset();
+  unsigned int tmp_number = (int) __bin_displaynumber;
+  BINLED1 = (tmp_number >> 0) & 1;
+  BINLED2 = (tmp_number >> 1) & 1;
+  BINLED3 = (tmp_number >> 2) & 1;
+  BINLED4 = (tmp_number >> 3) & 1;
+  BINLED5 = (tmp_number >> 4) & 1;
+  BINLED6 = (tmp_number >> 5) & 1;
+  BINLED_DOT = (((int) (__bin_displaynumber * 10.0F)) % 10) >= 5;
+}
+
+/**
+ * Resettet alle nötigen Ausgänge.
+ * Andere Output-Pins, die nicht benötigt werden,
+ * werden nicht zurückgesetzt!
+ */
+void _bdisplay_reset()
+{
+  BINLED_DOT = 0;
+  BINLED1 = 0;
+  BINLED2 = 0;
+  BINLED3 = 0;
+  BINLED4 = 0;
+  BINLED5 = 0;
+  BINLED6 = 0;
+}
---
+++ binary_display.h
@@ -0,0 +1,52 @@
+/* 
+ * File:   binary_display.h
+ * Author: Werner
+ *
+ * Dieses Display beschreibt die binäre Darstellung von 0-63.5.
+ * Hierzu werden 6+1 LEDs benötigt
+ * 
+ * Created on 11. Juni 2016, 12:40
+ */
+
+#ifndef BINARY_DISPLAY_H
+#define	BINARY_DISPLAY_H
+
+#include "main.h"
+#include <xc.h>
+
+/*
+ * Macros für Konfiguration 
+ */
+#define BINLED_DOT LATAbits.LATA1
+#define BINLED1    LATBbits.LATB2
+#define BINLED2    LATBbits.LATB1
+#define BINLED3    LATBbits.LATB0
+#define BINLED4    LATAbits.LATA4
+#define BINLED5    LATBbits.LATB3
+#define BINLED6    LATAbits.LATA2
+
+/**
+ * Setzt die Zahl, die angezeigt werden soll.
+ * Kommastellenbehandlung:
+ * Erste Kommastelle >= 5 -> LED an
+ * Erste Kommastelle < 5  -> LED aus
+ * 
+ * @param pNumber Zahl, die angezeigt werden soll
+ */
+void bdisplay_set(float pNumber);
+
+/**
+ * Setzt die OUTPUT-Pins auf den nötigen Zustand,
+ * damit die mit bdisplay_set gesetzte Zahl angezeigt wird
+ */
+void bdisplay_loop();
+
+/**
+ * Resettet alle nötigen Ausgänge.
+ * Andere Output-Pins, die nicht benötigt werden,
+ * werden nicht zurückgesetzt!
+ */
+void _bdisplay_reset();
+
+#endif	/* BINARY_DISPLAY_H */
+
